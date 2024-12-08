36b6da6ae2692d16608c82eab6b632f5c6542d12
VaradNerlekarUMD <nerlekar@umd.edu>
2024-12-08 19:37:28 +0000

    feat: end-effector pose plugin

--- surgical_tools_sorter/launch/ign_gazebo.launch.py
+++ surgical_tools_sorter/launch/ign_gazebo.launch.py
@@ -6,40 +6,49 @@ from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    # Set the environment variable for Ignition Gazebo resources
     os.environ["IGN_GAZEBO_RESOURCE_PATH"] = os.path.join(
         FindPackageShare('surgicalBot').find("surgical_tools_sorter"), 'models')
 
+    # Launch Ignition Gazebo with the specified world
     ign_gazebo_world = ExecuteProcess(
-        cmd=[[
-            'ign gazebo ',
+        cmd=[
+            'ignition', 'gazebo',
             PathJoinSubstitution([
                 FindPackageShare('surgical_tools_sorter'),
                 'worlds',
-                'tool_sorter_world.sdf '
+                'tool_sorter_world.sdf'
             ]),
-            '--render-engine ogre ',
-            '-v 4 '
-        ]],
+            '--render-engine', 'ogre',  # Set the rendering engine to Ogre
+            '-v', '4'  # Set verbosity level to 4 for more detailed logs
+        ],
         shell=True
     )
-    
+
+    # Launch RViz2 for visualization
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
     )
-    
+
+    # Launch the ROS 2 to Ignition bridge for the camera topic
     cam_topic_bridge = Node(
         package='ros_ign_bridge',
         executable='parameter_bridge',
         arguments=['/camera@sensor_msgs/msg/Image@ignition.msgs.Image'],
     )
-    
-    # controller_node = Node( 
+
+    # Example of a robot controller node (uncomment and configure as needed)
+    # controller_node = Node(
+    #     package='controller_manager',
+    #     executable='controller_spawner',
+    #     arguments=['your_controller_configurations_here'],
     # )
-    
+
+    # Return the launch description with all actions
     return launch.LaunchDescription([
         ign_gazebo_world,
         cam_topic_bridge,
         rviz_node,
-        # controller_node
+        # controller_node  # Uncomment and configure if you want to launch a controller node
     ])
